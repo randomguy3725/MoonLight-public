451ee3d091dd36fc573be914904324a6a4b553d2
Randumbguy <110034079+randomguy3725@users.noreply.github.com>
2024-11-29 17:10:38 +0800

    fix crash?

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -7,7 +7,7 @@ public class Start {
 
     public static void main(String[] args)
     {
-        System.setProperty("org.lwjgl.librarypath", new File("../natives/" + (System.getProperty("os.name").startsWith("Windows") ? "windows" : "linux")).getAbsolutePath());
+        //System.setProperty("org.lwjgl.librarypath", new File("../natives/" + (System.getProperty("os.name").startsWith("Windows") ? "windows" : "linux")).getAbsolutePath());
 
         Main.main(concat(new String[]{"--version", "GradleMCP", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
     }
