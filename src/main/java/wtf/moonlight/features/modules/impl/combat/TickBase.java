dbd9380582c75c900f360ec4ab30c11ae8e7219a
Randumbguy <110034079+randomguy3725@users.noreply.github.com>
2024-11-30 19:22:03 +0800

    test

--- src/main/java/wtf/moonlight/features/modules/impl/combat/TickBase.java
+++ src/main/java/wtf/moonlight/features/modules/impl/combat/TickBase.java
@@ -6,6 +6,7 @@ import net.minecraft.util.Vec3;
 import wtf.moonlight.events.annotations.EventTarget;
 import wtf.moonlight.events.impl.misc.TimerManipulationEvent;
 import wtf.moonlight.events.impl.player.MoveEvent;
+import wtf.moonlight.events.impl.player.UpdateEvent;
 import wtf.moonlight.events.impl.render.Render3DEvent;
 import wtf.moonlight.features.modules.Module;
 import wtf.moonlight.features.modules.ModuleCategory;
@@ -15,6 +16,7 @@ import wtf.moonlight.features.values.impl.ModeValue;
 import wtf.moonlight.features.values.impl.SliderValue;
 import wtf.moonlight.utils.math.MathUtils;
 import wtf.moonlight.utils.math.TimerUtils;
+import wtf.moonlight.utils.player.MovementUtils;
 import wtf.moonlight.utils.player.PlayerUtils;
 import wtf.moonlight.utils.player.RotationUtils;
 import wtf.moonlight.utils.player.SimulatedPlayer;
@@ -30,9 +32,9 @@ import java.util.stream.Stream;
 public class TickBase extends Module {
     public final SliderValue maxBalance = new SliderValue("Max Balance", 100, 0, 5000, 50, this);
     public final SliderValue delay = new SliderValue("Delay", 50, 0, 1000,50, this);
-    public final SliderValue minActiveRange = new SliderValue("Min Active Range", 3f, 0.1f, 7f, 0.1f, this);
-    public final SliderValue maxActiveRange = new SliderValue("Max Active Range", 7f, 0.1f, 7f, 0.1f, this);
     public final BoolValue autoSettings = new BoolValue("Auto",true,this);
+    public final SliderValue minActiveRange = new SliderValue("Min Active Range", 3f, 0.1f, 7f, 0.1f, this,() -> !autoSettings.get());
+    public final SliderValue maxActiveRange = new SliderValue("Max Active Range", 7f, 0.1f, 7f, 0.1f, this,() -> !autoSettings.get());
     public final SliderValue predictTicks = new SliderValue("Predict Ticks", 4, 1, 20, this,() -> !autoSettings.get());
     public final BoolValue displayPredictPos = new BoolValue("Dislay Predict Pos",false,this);
     public final BoolValue check = new BoolValue("Check",false,this);
@@ -47,16 +49,21 @@ public class TickBase extends Module {
             boolean shouldCharge = false;
 
             boolean shouldDischarge = shifted >= maxBalance.get();
+            
+            int value = (int) (autoSettings.get() ? maxBalance.get() / 20 : predictTicks.get()) - 1;
 
-            EntityOtherPlayerMP target = (EntityOtherPlayerMP) PlayerUtils.getTarget(maxActiveRange.get() * 3);
+            EntityOtherPlayerMP target = (EntityOtherPlayerMP) PlayerUtils.getTarget(autoSettings.get() ? predictProcesses.get(value).position.distanceTo(mc.thePlayer.getPositionVector()) : maxActiveRange.get() * 3);
 
             if (target != null &&
-                    predictProcesses.get((int) (predictTicks.get() - 1)).position.distanceTo(target.getPositionVector()) < mc.thePlayer.getPositionVector().distanceTo(target.getPositionVector()) &&
-                    (!autoSettings.get() && MathUtils.inBetween(minActiveRange.get(), maxActiveRange.get(), predictProcesses.get((int) (predictTicks.get() - 1)).position.distanceTo(target.getPositionVector())) ||
-                            autoSettings.get() && mc.thePlayer.getPositionVector().distanceTo(predictProcesses.get((int) (predictTicks.get() - 1)).position) > 3)
+                    predictProcesses.get(value).position.distanceTo(target.getPositionVector()) < mc.thePlayer.getPositionVector().distanceTo(target.getPositionVector()) &&
+                    (!autoSettings.get() &&
+                            MathUtils.inBetween(minActiveRange.get(), maxActiveRange.get(), predictProcesses.get(value).position.distanceTo(target.getPositionVector())) ||
+                            autoSettings.get() &&
+                                    MathUtils.inBetween(3, mc.thePlayer.getPositionVector().distanceTo(target.getPositionVector()), predictProcesses.get(value).position.distanceTo(target.getPositionVector())))
                     &&
+
                     mc.thePlayer.canEntityBeSeen(target) && target.canEntityBeSeen(mc.thePlayer) &&
-                    (RotationUtils.getRotationDifference(mc.thePlayer, target) <= 90 && check.get() || !check.get()) && !checks() && !predictProcesses.get((int) (predictTicks.get() - 1)).isCollidedHorizontally) {
+                    (RotationUtils.getRotationDifference(mc.thePlayer, target) <= 90 && check.get() || !check.get()) && !checks() && !predictProcesses.get(value).isCollidedHorizontally) {
                 shouldCharge = shifted < maxBalance.get();
             }
 
@@ -78,32 +85,34 @@ public class TickBase extends Module {
     }
 
     @EventTarget
-    public void onMove(MoveEvent event) {
+    public void onUpdate(UpdateEvent event) {
 
         predictProcesses.clear();
+        if(MovementUtils.isMoving()) {
 
-        SimulatedPlayer simulatedPlayer = SimulatedPlayer.fromClientPlayer(mc.thePlayer.movementInput);
-        
-        for (int i = 0; i < (autoSettings.get() ? maxBalance.get() / 20 : predictTicks.get()); i++) {
-            simulatedPlayer.tick();
-            predictProcesses.add(new PredictProcess(
-                    simulatedPlayer.getPos(),
-                    simulatedPlayer.fallDistance,
-                    simulatedPlayer.motionX,
-                    simulatedPlayer.motionY,
-                    simulatedPlayer.motionZ,
-                    simulatedPlayer.onGround,
-                    simulatedPlayer.isCollidedHorizontally
-            ));
+            SimulatedPlayer simulatedPlayer = SimulatedPlayer.fromClientPlayer(mc.thePlayer.movementInput);
+
+            for (int i = 0; i < (int) (autoSettings.get() ? maxBalance.get() / 20 : predictTicks.get()); i++) {
+                simulatedPlayer.tick();
+                predictProcesses.add(new PredictProcess(
+                        simulatedPlayer.getPos(),
+                        simulatedPlayer.fallDistance,
+                        simulatedPlayer.motionX,
+                        simulatedPlayer.motionY,
+                        simulatedPlayer.motionZ,
+                        simulatedPlayer.onGround,
+                        simulatedPlayer.isCollidedHorizontally
+                ));
+            }
         }
     }
 
     @EventTarget
     public void onRender3D(Render3DEvent event) {
         if(displayPredictPos.get()) {
-            double x = predictProcesses.get((int) ((autoSettings.get() ? maxBalance.get() / 20 : predictTicks.get())) - 1).position.xCoord - mc.getRenderManager().viewerPosX;
-            double y = predictProcesses.get((int) ((autoSettings.get() ? maxBalance.get() / 20 : predictTicks.get())) - 1).position.yCoord - mc.getRenderManager().viewerPosY;
-            double z = predictProcesses.get((int) ((autoSettings.get() ? maxBalance.get() / 20 : predictTicks.get())) - 1).position.zCoord - mc.getRenderManager().viewerPosZ;
+            double x = predictProcesses.get((int) (autoSettings.get() ? maxBalance.get() / 20 : predictTicks.get()) - 1).position.xCoord - mc.getRenderManager().viewerPosX;
+            double y = predictProcesses.get((int) (autoSettings.get() ? maxBalance.get() / 20 : predictTicks.get()) - 1).position.yCoord - mc.getRenderManager().viewerPosY;
+            double z = predictProcesses.get((int) (autoSettings.get() ? maxBalance.get() / 20 : predictTicks.get()) - 1).position.zCoord - mc.getRenderManager().viewerPosZ;
             AxisAlignedBB box = mc.thePlayer.getEntityBoundingBox().expand(0.1D, 0.1, 0.1);
             AxisAlignedBB axis = new AxisAlignedBB(box.minX - mc.thePlayer.posX + x, box.minY - mc.thePlayer.posY + y, box.minZ - mc.thePlayer.posZ + z, box.maxX - mc.thePlayer.posX + x, box.maxY - mc.thePlayer.posY + y, box.maxZ - mc.thePlayer.posZ + z);
             RenderUtils.drawAxisAlignedBB(axis,false, true, new Color(50, 255, 255, 150).getRGB());
