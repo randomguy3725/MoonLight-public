103339f4241c7a62b41c23f0dbc8b600b327fecc
Randumbguy <110034079+randomguy3725@users.noreply.github.com>
2024-12-24 12:07:53 +0800

    test backtrack

--- src/main/java/wtf/moonlight/features/modules/impl/combat/BackTrack.java
+++ src/main/java/wtf/moonlight/features/modules/impl/combat/BackTrack.java
@@ -61,6 +61,7 @@ public class BackTrack extends Module {
                 if (spoofed) {
                     PingSpoofComponent.disable();
                     PingSpoofComponent.dispatch();
+                    spoofed = false;
                 }
                 return;
             }
@@ -69,6 +70,7 @@ public class BackTrack extends Module {
                 if (spoofed) {
                     PingSpoofComponent.disable();
                     PingSpoofComponent.dispatch();
+                    spoofed = false;
                 }
                 return;
             }
