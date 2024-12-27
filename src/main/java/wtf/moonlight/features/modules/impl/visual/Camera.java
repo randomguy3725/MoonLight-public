c575097c7813faac1b77866f08dd5176c55c70de
SyCC_Shark <123637009+SyCC1337@users.noreply.github.com>
2024-12-27 16:42:16 +0800

    Add Custom 3rdPersonDistance

--- src/main/java/wtf/moonlight/features/modules/impl/visual/Camera.java
+++ src/main/java/wtf/moonlight/features/modules/impl/visual/Camera.java
@@ -1,3 +1,13 @@
+/*
+ * MoonLight Hacked Client
+ *
+ * A free and open-source hacked client for Minecraft.
+ * Developed using Minecraft's resources.
+ *
+ * Repository: https://github.com/randomguy3725/MoonLight
+ *
+ * Author(s): [Randumbguy & opZywl & lucas]
+ */
 package wtf.moonlight.features.modules.impl.visual;
 
 import net.minecraft.client.gui.ScaledResolution;
