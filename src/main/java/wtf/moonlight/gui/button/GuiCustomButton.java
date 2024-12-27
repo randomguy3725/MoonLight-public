2ecf256b54a023fd1eef38b32911b69719952f2c
Randumbguy <110034079+randomguy3725@users.noreply.github.com>
2024-12-27 15:47:28 +0800

    sdvdsvdsf

--- src/main/java/wtf/moonlight/gui/button/GuiCustomButton.java
+++ src/main/java/wtf/moonlight/gui/button/GuiCustomButton.java
@@ -1,4 +1,14 @@
- wtf.moonlight.gui.button;
+/*
+ * MoonLight Hacked Client
+ *
+ * A free and open-source hacked client for Minecraft.
+ * Developed using Minecraft's resources.
+ *
+ * Repository: https://github.com/randomguy3725/MoonLight
+ *
+ * Author(s): [RandomGuy & opZywl]
+ */
+package wtf.moonlight.gui.button;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
