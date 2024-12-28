2bc282dd1430fd58aee7d37d8b5c17fdd957e46a
eonianisnoob <158603026+eonianisnoob@users.noreply.github.com>
2024-12-28 09:55:59 +0200

    Update Interface.java

--- src/main/java/wtf/moonlight/features/modules/impl/visual/Interface.java
+++ src/main/java/wtf/moonlight/features/modules/impl/visual/Interface.java
@@ -178,7 +178,7 @@ public class Interface extends Module {
                     boolean shouldChange = RenderUtils.COLOR_PATTERN.matcher(clientName.get()).find();
                     String text = shouldChange ? "§r" + clientName.get() : clientName.get().charAt(0) + "§r§f" + clientName.get().substring(1) +
                             "§7[§f" + Minecraft.getDebugFPS() + " FPS§7]§r ";
-                    Fonts.Tahoma.get(15).drawString
+                    Fonts.Tahoma.get(15).drawString;
                     break;
                 case "Type 1":
                     float posX = 4.0F;
